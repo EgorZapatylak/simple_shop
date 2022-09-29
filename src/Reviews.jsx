2cdb6fca4fc0ec9c8a7ba497f90a132a952c4fd6
EgorZapatylak <zapatylak96a@gmail.com>
2022-09-29 22:29:52 +0300

    Created array with start data in Reviews.jsx components

--- src/Reviews.jsx
+++ src/Reviews.jsx
@@ -1,12 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+import avatarIcon from './assets/img/avatarIcon.svg'
 
 export const Reviews = () => {
+    const [reviews, setReviews] = useState([
+        {
+            author: 'Jane Cooper',
+            title: 'Amazing Product',
+            text: 'Lorem Ipsum is simply dummy text of the printing and ' +
+                'typesetting industry. Lorem Ipsum has been the industry',
+            date: '01/01/2021',
+            rating: 4
+        },
+        {
+            author: 'Max Doodle',
+            title: 'Best choice',
+            text: 'Various versions have evolved over the years,' +
+                'sometimes by accident,sometimes on purpose' +
+                '(injected humour and the like).',
+            date: '05/23/2021',
+            rating: 5
+},
+])
     return (
-        <div className="review">
-            <h3>Reviews (189)</h3>
-            <textarea placeholder="Provide your text..."></textarea>
-            <button>Send review</button>
-        </div>
+        <>
+            <div className="review">
+                <h3>Reviews (189)</h3>
+                <textarea placeholder="Provide your text..."></textarea>
+                <button>Send review</button>
+            </div>
+            <div>
+                {reviews.map((r) => {
+                    return (
+                        <div className="reviewField">
+                            <div className="info">
+                                <div className="user">
+                                    <img src={avatarIcon} alt="" />
+                                    <div className="infoBox">
+                                        <p className="author">{r.author}</p>
+                                        <p className="rating">{r.rating} Rating</p>
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className="date">{r.date}</p>
+                                </div>
+                            </div>
+
+                            <div className="content">
+                                <p className="title">{r.title}</p>
+                                <p>{r.text}</p>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
     );
 };
 
